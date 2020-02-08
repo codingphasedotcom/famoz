3df7f6bdbfdbfb557ca286570a29edcc25864526
joe santos garcia <joesantosgarcia@gmail.com>
2020-02-08 10:28:25 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts/main-layout')
+
+@section('title', 'Page Title')
+
+@section('content')
+    <div id="home-content">
+
+        <div class="home-jumbo">
+            <div class="title">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Trending Today</h2>
+                <div class="trending-list">
+                    <div class="video">
+                        <div class="number">01</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">02</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">03</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">04</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">05</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">06</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">07</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                    <div class="video">
+                        <div class="number">08</div>
+                        <div class="video-image"></div>
+                        <div class="info">
+                            <div class="title">Baje Con Trenza Remix</div>
+                            <div class="artist">El Cherry Scom, Kiko El Crazy, Ozuna
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h2>Editors Choice</h2>
+                <div class="editors-list">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="box-user">
+                                </div>
+                                <div class="box-user">
+                                </div>
+                                
+                            </div>
+                            <div class="col-md-6">
+                                <div class="box-user">
+                                </div>
+                                <div class="box-user">
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h2>Editors Choice</h2>
+                <div class="editors-list">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="box-user">
+                                </div>
+                                <div class="box-user">
+                                </div>
+                                
+                            </div>
+                            <div class="col-md-6">
+                                <div class="box-user">
+                                </div>
+                                <div class="box-user">
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+@endsection
---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+        <link href="/css/app.css" rel="stylesheet">
+        <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    </head>
+    <body>
+        <div id="main-layout">
+            <div class="side-menu">
+                <div class="logo">
+                Famoz
+                </div>
+                <div class="links">
+                    <a href="#"> <ion-icon name="globe"></ion-icon> Explore</a>
+                    <a href="#"><ion-icon name="stats-chart-sharp"></ion-icon> Trending</a>
+                    <a href="#"> <ion-icon name="radio"></ion-icon>Genres</a>
+                    <a href="#">Dembow</a>
+                </div>
+            </div>
+            <div class="content-area">
+                @yield('content')
+                <div class="container">
+            
+            <footer class="pt-4 my-md-5 pt-md-5 border-top">
+                <div class="row">
+                <div class="col-12 col-md">
+                    <img class="mb-2" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                    <small class="d-block mb-3 text-muted">© 2017-2019</small>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Features</h5>
+                    <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Cool stuff</a></li>
+                    <li><a class="text-muted" href="#">Random feature</a></li>
+                    <li><a class="text-muted" href="#">Team feature</a></li>
+                    <li><a class="text-muted" href="#">Stuff for developers</a></li>
+                    <li><a class="text-muted" href="#">Another one</a></li>
+                    <li><a class="text-muted" href="#">Last time</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Resources</h5>
+                    <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Resource</a></li>
+                    <li><a class="text-muted" href="#">Resource name</a></li>
+                    <li><a class="text-muted" href="#">Another resource</a></li>
+                    <li><a class="text-muted" href="#">Final resource</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Team</a></li>
+                    <li><a class="text-muted" href="#">Locations</a></li>
+                    <li><a class="text-muted" href="#">Privacy</a></li>
+                    <li><a class="text-muted" href="#">Terms</a></li>
+                    </ul>
+                </div>
+                </div>
+            </footer>
+            </div>
+        </div>
+        </div>
+    </body>
+</html>
