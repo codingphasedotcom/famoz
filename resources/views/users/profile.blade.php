86ff9239cd540afa6edf211388a1eb1e7a934002
joe santos garcia <joesantosgarcia@gmail.com>
2020-02-25 01:44:58 -0500

    Working on the profile page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -7,7 +7,7 @@
     <div id="profile-content">
         <section id="top-section">
             <div class="user-image">
-                <img src="https://i1.sndcdn.com/avatars-000720436693-hfwlc7-t500x500.jpg">
+                <img loading="lazy" src="https://i1.sndcdn.com/avatars-000720436693-hfwlc7-t500x500.jpg">
             </div>
             <div class="profile-information">
                 <div class="user-title">
@@ -38,124 +38,160 @@
                 </div>
             </div>
         </section>
-        <section id="cast-section"> 
-            <div class="row">
-                <div class="col-sm-3 col-md-4">
-                    <h3>dembowBaje Contrenza - El Cherry Scom Ft. Ozuna, Kiko El Crazy</h3>
-                </div>
-                <div class="col-sm-3 col-md-2">
-                    <h6>Vote</h6>
-                    <div class="vote-btn">
-                        <i class="fas fa-heart"></i>
+        <section id="profile-tabs">
+            <div class="tab-menu">
+                <div class="tab active">Videos</div>
+                <div class="tab">Status</div>
+            </div>
+            <div class="tab-content">
+                <section class="tab video-feed active">
+                    <div class="row">
+                        <div class="col-md-12 col-lg-9">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="video-post">
+                                        <a href="/video/baje-con-trenza">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <a href="/video/baje-con-trenza" class="video-title">Baje Con Trenza</a>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="col-md-12 col-lg-3">
+                            <div class="video-post">
+                                
+                            </div>
+                        </div>
                     </div>
-
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <h6>Crew</h6>
-                    <div class="cast-members">
-                        <div class="member">
-                            <img src="https://randomuser.me/api/portraits/men/32.jpg" class="user-image">
-                            <span class="title">Artist</span>
+                </section>
+                <section class="tab status-feed">
+                    <div class="row">
+                        <div class="col-md-12 col-lg-9">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="video-post">
+                                        <a href="#">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <span class="video-title">Baje Con Trenza</span>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="video-post">
+                                        <a href="#">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <span class="video-title">Baje Con Trenza</span>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="video-post">
+                                        <a href="#">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <span class="video-title">Baje Con Trenza</span>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="video-post">
+                                        <a href="#">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <span class="video-title">Baje Con Trenza</span>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="video-post">
+                                        <a href="#">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <span class="video-title">Baje Con Trenza</span>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="video-post">
+                                        <a href="#">
+                                            <img loading="lazy" src="https://www.rollingstone.com/wp-content/uploads/2019/12/Ozuna-1.jpg">
+                                        </a>
+                                        <span class="video-title">Baje Con Trenza</span>
+                                        <span class="video-info">12,988 views</span>
+                                    </div>
+                                </div>
+                            </div>
+                            
                         </div>
-                        <div class="member">
-                            <img src="https://tinyfac.es/data/avatars/7D3FA6C0-83C8-4834-B432-6C65ED4FD4C3-500w.jpeg" class="user-image">
-                            <span class="title">Artist</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNTk2OGU4NzktODhhOC00Nzc2LWIyNzYtOWViMjljZGFiNTMxXkEyXkFqcGdeQXVyMTE1NTQwOTk@._V1_UY256_CR12,0,172,256_AL_.jpg" class="user-image">
-                            <span class="title">Producer</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://tinyfac.es/data/avatars/AEF44435-B547-4B84-A2AE-887DFAEE6DDF-200w.jpeg" class="user-image">
-                            <span class="title">Director</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwMzc5OTEzOF5BMl5BanBnXkFtZTgwMDc5ODU3MTE@._V1_UX172_CR0,0,172,256_AL_.jpg" class="user-image">
-                            <span class="title">Model</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://tinyfac.es/data/avatars/8B510E03-96BA-43F0-A85A-F38BB3005AF1-500w.jpeg" class="user-image">
-                            <span class="title">Model</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwMzc5OTEzOF5BMl5BanBnXkFtZTgwMDc5ODU3MTE@._V1_UX172_CR0,0,172,256_AL_.jpg" class="user-image">
-                            <span class="title">Model</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://tinyfac.es/data/avatars/8B510E03-96BA-43F0-A85A-F38BB3005AF1-500w.jpeg" class="user-image">
-                            <span class="title">Model</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwMzc5OTEzOF5BMl5BanBnXkFtZTgwMDc5ODU3MTE@._V1_UX172_CR0,0,172,256_AL_.jpg" class="user-image">
-                            <span class="title">Model</span>
-                        </div>
-                        <div class="member">
-                            <img src="https://tinyfac.es/data/avatars/8B510E03-96BA-43F0-A85A-F38BB3005AF1-500w.jpeg" class="user-image">
-                            <span class="title">Model</span>
+                        <div class="col-md-12 col-lg-3">
+                            <div class="video-post">
+                                
+                            </div>
                         </div>
                     </div>
-                </div>
+                </section>
                 
-            </div> 
+            </div>
         </section>
-        
-        <div class="row">
-            <div class="col-md-6 order-sm-2 order-md-1">
-                <h5>Comments</h5>
-                <div class="comments">
-                    <div class="comment">
-                        <div class="user-image"></div>
-                        <div class="info">
-                            <div class="name">Billy</div>
-                            <div class="message">El Cherry Scom, Kiko El Crazy, Ozuna
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 order-sm-1 order-md-2">
-                <h5>More From This Artist</h5>
-                <div class="editors-list">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="box-user">
-                                </div>
-                                <div class="box-user">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="box-user">
-                                </div>
-                                <div class="box-user">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <h5>More From This Artist</h5>
-                <div class="editors-list">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="box-user">
-                                </div>
-                                <div class="box-user">
-                                </div>
-                                
-                            </div>
-                            <div class="col-md-6">
-                                <div class="box-user">
-                                </div>
-                                <div class="box-user">
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     
 @endsection
